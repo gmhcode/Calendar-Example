d42eaab6f5614f33f35e26d27ef5dc2f3a4cedc5
Greg Hughes <greghughes988@gmail.com>
2019-01-19 20:22:33 -0700

    found a good place for the dot to be. found method to extract all dates and events

--- CalendarTest/ViewController.swift
+++ CalendarTest/ViewController.swift
@@ -5,7 +5,16 @@
 //  Created by Greg Hughes on 1/19/19.
 //  Copyright © 2019 Greg Hughes. All rights reserved.
 //
-
+extension Date{
+    
+    var asString: String{
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        return formatter.string(from: self)
+    }
+    
+}
 import UIKit
 import JTAppleCalendar
 class ViewController: UIViewController {
@@ -76,6 +85,7 @@ class ViewController: UIViewController {
             formatter.date(from: "2019 02 04")!: "Hello",
             formatter.date(from: "2019 02 03")!: "Hello"
         ]
+        
     }
     
     
@@ -128,11 +138,11 @@ class ViewController: UIViewController {
         // MARK: - Calander Dot activates if we get info from server
     }
     
-    func configureCell(cell: JTAppleCell?, cellState: CellState){
+    func cellDotsAppear(cell: JTAppleCell?, cellState: CellState){
         
         guard let cell = cell as? CustomCell else { return }
         
-        formatter.dateFormat = "yyy MM dd"
+        formatter.dateFormat = "yyyy MM dd"
         
         handleCellEvents(cell: cell, cellState: cellState)
         
@@ -151,7 +161,7 @@ extension ViewController: JTAppleCalendarViewDelegate{
         handleCellTextColor(view: cell, cellState: cellState)
         // MARK: - Calendar resets cells when moving between months
         handleCellSelected(view:  cell, cellState: cellState)
-        
+        cellDotsAppear(cell: cell, cellState: cellState)
         
         return cell
     }
@@ -159,21 +169,60 @@ extension ViewController: JTAppleCalendarViewDelegate{
     // MARK: - Calendar color change when selecting and de-selecting
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
         
+        
+        //play with dates here, configure
+        print(date.asString)
+        print(date)
+        
         handleCellSelected(view:  cell, cellState: cellState)
         handleCellTextColor(view: cell, cellState: cellState)
+        cellDotsAppear(cell: cell, cellState: cellState)
         
     }
+    
+    
     func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
         
         handleCellSelected(view:  cell, cellState: cellState)
-        handleCellTextColor(view: cell, cellState: cellState)    }
+        handleCellTextColor(view: cell, cellState: cellState)
+        cellDotsAppear(cell: cell, cellState: cellState)
+    }
     
     func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
         setUpViewsOfCalendar(from: visibleDates)
         
+       
+        var dateArray = printVisibleDates(visibleDatesMonthDates: visibleDates.monthDates)
+        
+        print(" 😘 \(dateArray) ")
+        
+        //visible dates is 3 arrays of dictionaries (inDates, monthDates, outDates)
+        //we need to find a way to check monthDates list all the dates that have events assigned to them
+        
+        //TODO: for each date
+        
+        }
+    
+
+    //use this to extract all the dates from the month
+    func printVisibleDates(visibleDatesMonthDates: [(Date,IndexPath)]) -> [Date]{
+        
+        var i = 0
+        var dateArray : [Date] = []
+        while i < visibleDatesMonthDates.count{
+            print(visibleDatesMonthDates[i].0)
+            dateArray.append(visibleDatesMonthDates[i].0)
+            i += 2
+        }
+        return dateArray
+    }
+    
+    //extract all dates with events from array
+    func datesWithEvents(dates: [Date]){
+        
+        
     }
 }
-
 
 extension ViewController: JTAppleCalendarViewDataSource{
     
@@ -191,7 +240,7 @@ extension ViewController: JTAppleCalendarViewDataSource{
         formatter.locale = Calendar.current.locale
         
         let startDate = formatter.date(from: "2017 02 01")
-        let endDate = formatter.date(from: "2019 12 31")
+        let endDate = formatter.date(from: "2200 12 31")
         
         guard let sDate = startDate, let eDate = endDate else { return ConfigurationParameters(startDate: Date(), endDate: Date()) }
         // MARK: - showing buggy Date
